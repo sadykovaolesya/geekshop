d1fb5db16506ae756c9707b4250d321340cc2a7c
Olesya <progafox@gmail.com>
2020-10-10 20:02:02 +0500

    Lesson 4. User login and logout

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -5,13 +5,17 @@ from django.conf.urls.static import static
 from django.conf.urls import include
 import mainapp.views as mainapp
 
-urlpatterns =[
+urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", mainapp.main, name="main" ),
-    path("products/",include("mainapp.urls", namespace="products")),
+    path("", mainapp.main, name="main"),
+    path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
-
+    path("auth/", include("authnapp.urls", namespace="auth")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
