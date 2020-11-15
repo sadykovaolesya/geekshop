b5da6450b3404a118c20126f1d81f567cbcf43e3
Olesya <progafox@gmail.com>
2020-11-15 13:52:03 +0500

    lesson_5. Common fixes

--- mainapp/models.py
+++ mainapp/models.py
@@ -18,10 +18,15 @@ class Product(models.Model):
     description = models.TextField(verbose_name="описание продукта", blank=True)
     price = models.DecimalField(verbose_name="цена продукта", max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name="количество на складе", default=0)
+    reserved = models.PositiveIntegerField(verbose_name="зарезервировано", default=0)
     is_active = models.BooleanField(verbose_name="продукт активен", default=True)
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
+
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_active=True).order_by("category", "name")
 
 
 class Contact(models.Model):
